6edccf208fe9367c7cdde7e62c6bbcc550761041
1dg <119858304+ricky0601@users.noreply.github.com>
2026-03-09 11:11:44 +0900

    chore: 로아끼욧 앱 아이콘 및 PWA manifest 업데이트

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+if (process.env.NODE_ENV !== 'development') {
+  console.log = () => {};
+  console.warn = () => {};
+  console.error = () => {};
+  console.info = () => {};
+  console.debug = () => {};
+}
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
